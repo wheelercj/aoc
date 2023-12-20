58dcd213dc6478ecfe784fd149c5734c34b177e2
wheelercj <chrisw289@gmail.com>
2023-12-19 21:39:50 -0800

    Add solution to 2023/6 part 1

---
+++ 2023/6/go.mod
@@ -0,0 +1,3 @@
+module github.com/wheelercj/aoc/2023/6
+
+go 1.21.5
---
+++ 2023/6/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type mm int // millimeters
+type ms int // milliseconds
+
+type Race struct {
+	timeLimit   ms
+	recordDist  mm
+	winWayCount int
+}
+
+func main() {
+	races := parseInput("input.txt")
+	// fmt.Println("races:", races)
+	part1(races)
+}
+
+func parseInput(fileName string) []Race {
+	bytes, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(bytes), "\r\n")
+	timesStr, distancesStr := lines[0], lines[1]
+	times, distances := getInts(timesStr), getInts(distancesStr)
+
+	var races []Race
+	for i := 0; i < len(times); i++ {
+		races = append(
+			races,
+			Race{timeLimit: ms(times[i]), recordDist: mm(distances[i])},
+		)
+	}
+
+	return races
+}
+
+func getInts(numsStr string) []int {
+	var nums []int
+	numsStrs := strings.Fields(numsStr)
+	for i := 1; i < len(numsStrs); i++ {
+		nums = append(nums, mustParseInt(numsStrs[i]))
+	}
+	return nums
+}
+
+func mustParseInt(numStr string) int {
+	num, err := strconv.ParseInt(numStr, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return int(num)
+}
+
+func part1(races []Race) {
+	for i, race := range races {
+		fmt.Printf("race %d\n", i+1)
+		races[i].winWayCount = getWinWayCount(race)
+	}
+
+	product := 1
+	for _, race := range races {
+		product *= race.winWayCount
+	}
+
+	fmt.Println("product of win way counts:", product)
+}
+
+func getWinWayCount(race Race) int {
+	min, err := findMinChargeTimeNeeded(race)
+	if err != nil {
+		panic(err)
+	}
+	max, err := findMaxChargeTimeNeeded(race)
+	if err != nil {
+		panic(err)
+	}
+	return int(max-min) + 1
+}
+
+// findMinChargeTimeNeeded performs a binary search to find the minimum charge time
+// required to beat the race's record.
+func findMinChargeTimeNeeded(race Race) (ms, error) {
+	startTime := ms(0)            // start of where to search, not necessarily start of race
+	endTime := ms(race.timeLimit) // end of where to search, not necessarily end of race
+	for startTime < endTime-1 {
+		midTime := ms((endTime-startTime)/2 + startTime)
+		dist1 := getDist(midTime, race.timeLimit)
+		dist2 := getDist(midTime+1, race.timeLimit)
+		if dist1 <= race.recordDist && dist2 > race.recordDist {
+			return midTime + 1, nil
+		} else if dist1 > race.recordDist {
+			endTime = midTime + 1
+		} else if dist2 < race.recordDist {
+			startTime = midTime
+		}
+	}
+
+	return 0, fmt.Errorf("No minimum charge time to beat the record found.")
+}
+
+// findMaxChargeTimeNeeded performs a binary search to find the maximum charge time (in
+// milliseconds) required to beat the race's record.
+func findMaxChargeTimeNeeded(race Race) (ms, error) {
+	startTime := ms(0)            // start of where to search, not necessarily start of race
+	endTime := ms(race.timeLimit) // end of where to search, not necessarily end of race
+	for startTime < endTime-1 {
+		midTime := ms((endTime-startTime)/2 + startTime)
+		dist1 := getDist(midTime, race.timeLimit)
+		dist2 := getDist(midTime+1, race.timeLimit)
+		if dist1 > race.recordDist && dist2 <= race.recordDist {
+			return midTime, nil
+		} else if dist1 < race.recordDist {
+			endTime = midTime + 1
+		} else if dist2 > race.recordDist {
+			startTime = midTime
+		}
+	}
+
+	return 0, fmt.Errorf("No maximum charge time to beat the record found.")
+}
+
+// getDist determines the distance (in millimeters) the boat can travel after
+// timeCharged milliseconds spent charging during a race that is a total of timeLimit
+// milliseconds long.
+func getDist(timeCharged, timeLimit ms) mm {
+	return mm(timeCharged * (timeLimit - timeCharged))
+}
