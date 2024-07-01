faf2e2fe49e38b214de57b647f1c9ada20f1204e
wheelercj <chrisw289@gmail.com>
2024-06-30 20:19:57 -0700

    Finish day 13 part 2

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -16,8 +16,11 @@ func main() {
 
 	patterns := parseInput(string(content))
 
-	result := part1(patterns)
-	fmt.Println("part 1 result:", result)
+	result1 := getMirrorsTotal(patterns, findMirror)
+	fmt.Println("part 1 result:", result1)
+
+	result2 := getMirrorsTotal(patterns, findMirrorPart2)
+	fmt.Println("part 2 result:", result2)
 }
 
 func parseInput(content string) [][]string {
@@ -32,7 +35,7 @@ func parseInput(content string) [][]string {
 	return patterns
 }
 
-func part1(patterns [][]string) int {
+func getMirrorsTotal(patterns [][]string, findMirror func([]string) (int, int)) int {
 	var result int
 	for _, pattern := range patterns {
 		row, col := findMirror(pattern)
@@ -50,7 +53,7 @@ func findMirror(pattern []string) (row, col int) {
 	// search for 2 identical rows
 	for y := 0; y < len(pattern)-1; y++ {
 		if isMirrorAtRow(pattern, y) {
-			fmt.Printf("found mirror between rows %d and %d\n", y+1, y+2)
+			// fmt.Printf("found mirror between rows %d and %d\n", y+1, y+2)
 			return y + 1, 0
 		}
 	}
@@ -58,7 +61,7 @@ func findMirror(pattern []string) (row, col int) {
 	// search for 2 identical columns
 	for x := 0; x < len(pattern[0])-1; x++ {
 		if isMirrorAtCol(pattern, x) {
-			fmt.Printf("found mirror between cols %d and %d\n", x+1, x+2)
+			// fmt.Printf("found mirror between cols %d and %d\n", x+1, x+2)
 			return 0, x + 1
 		}
 	}
@@ -122,4 +125,96 @@ func colsMatch(pattern []string, x1, x2 int) bool {
 	}
 
 	return true
+}
+
+func findMirrorPart2(pattern []string) (row, col int) {
+	// search for 2 identical rows
+	for y := 0; y < len(pattern)-1; y++ {
+		diffCount := isMirrorAtRowPart2(pattern, y)
+		if diffCount == 1 {
+			// fmt.Printf("found mirror between rows %d and %d\n", y+1, y+2)
+			return y + 1, 0
+		}
+	}
+
+	// search for 2 identical columns
+	for x := 0; x < len(pattern[0])-1; x++ {
+		diffCount := isMirrorAtColPart2(pattern, x)
+		if diffCount == 1 {
+			// fmt.Printf("found mirror between cols %d and %d\n", x+1, x+2)
+			return 0, x + 1
+		}
+	}
+
+	panic("No mirror found")
+}
+
+func isMirrorAtRowPart2(pattern []string, y int) (diffCount int) {
+	diffCount = rowsDiffCount(pattern, y, y+1)
+	if diffCount > 1 {
+		return diffCount
+	}
+
+	y1 := y
+	y2 := y + 1
+	for {
+		y1--
+		y2++
+		if y1 < 0 || y2 >= len(pattern) {
+			return diffCount
+		}
+		diffCount += rowsDiffCount(pattern, y1, y2)
+		if diffCount > 1 {
+			return diffCount
+		}
+	}
+}
+
+func rowsDiffCount(pattern []string, y1, y2 int) (diffCount int) {
+	for x, ch1 := range pattern[y1] {
+		if ch1 != rune(pattern[y2][x]) {
+			if diffCount == 0 {
+				diffCount = 1
+			} else {
+				return 2
+			}
+		}
+	}
+
+	return diffCount
+}
+
+func isMirrorAtColPart2(pattern []string, x int) (diffCount int) {
+	diffCount = colsDiffCount(pattern, x, x+1)
+	if diffCount > 1 {
+		return diffCount
+	}
+
+	x1 := x
+	x2 := x + 1
+	for {
+		x1--
+		x2++
+		if x1 < 0 || x2 >= len(pattern[0]) {
+			return diffCount
+		}
+		diffCount += colsDiffCount(pattern, x1, x2)
+		if diffCount > 1 {
+			return diffCount
+		}
+	}
+}
+
+func colsDiffCount(pattern []string, x1, x2 int) (diffCount int) {
+	for _, row := range pattern {
+		if row[x1] != row[x2] {
+			if diffCount == 0 {
+				diffCount = 1
+			} else {
+				return 2
+			}
+		}
+	}
+
+	return diffCount
 }
