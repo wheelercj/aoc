2c0aa05c0836072ad8960df0ef3fa73ea0afd499
wheelercj <chrisw289@gmail.com>
2025-12-03 22:34:48 -0800

    Simplify 2025 day 4

--- 2025/4/1.py
+++ 2025/4/1.py
@@ -7,36 +7,28 @@ def main():
     for y, line in enumerate(lines):
         for x, ch in enumerate(line):
             if ch == "@":
-                surrounding: int = count_surrounding_rolls(x, y, lines)
-                if surrounding < 4:
+                neighbors: int = count_neighbor_rolls(x, y, lines)
+                if neighbors < 4:
                     accessible += 1
 
     print(accessible)
 
 
-def count_surrounding_rolls(x: int, y: int, lines: list[str]) -> int:
+def count_neighbor_rolls(x: int, y: int, lines: list[str]) -> int:
     count: int = 0
 
-    if x > 0:
-        if lines[y][x - 1] == "@":
-            count += 1
-    if x < len(lines[y]) - 1:
-        if lines[y][x + 1] == "@":
-            count += 1
-    if y > 0:
-        if lines[y - 1][x] == "@":
-            count += 1
-        if x > 0 and lines[y - 1][x - 1] == "@":
-            count += 1
-        if x < len(lines[y]) - 1 and lines[y - 1][x + 1] == "@":
-            count += 1
-    if y < len(lines) - 1:
-        if lines[y + 1][x] == "@":
-            count += 1
-        if x > 0 and lines[y + 1][x - 1] == "@":
-            count += 1
-        if x < len(lines[y]) - 1 and lines[y + 1][x + 1] == "@":
-            count += 1
+    for j in range(y - 1, y + 2):
+        if j < 0 or j >= len(lines):
+            continue
+
+        for i in range(x - 1, x + 2):
+            if i < 0 or i >= len(lines[j]):
+                continue
+            elif i == x and j == y:
+                continue
+
+            if lines[j][i] == "@":
+                count += 1
 
     return count
 
--- 2025/4/2.py
+++ 2025/4/2.py
@@ -3,50 +3,40 @@ def main():
         s: str = file.read().strip()
     lines: list[list[str]] = [list(line) for line in s.splitlines()]
 
-    removed: int = 0
     q: list[tuple[int, int]] = []
     for y, line in enumerate(lines):
         for x, ch in enumerate(line):
             if ch == "@":
                 q.append((x, y))
 
+    removed: int = 0
     while q:
-        coord: tuple[int, int] = q.pop(0)
-        x: int = coord[0]
-        y: int = coord[1]
+        x, y = q.pop(0)
         if lines[y][x] == "@":
-            surrounding: list[tuple[int, int]] = get_surrounding_rolls(x, y, lines)
-            if len(surrounding) < 4:
+            neighbors: list[tuple[int, int]] = get_neighbor_rolls(x, y, lines)
+            if len(neighbors) < 4:
                 removed += 1
                 lines[y][x] = "."
-                q.extend(surrounding)
+                q.extend(neighbors)
 
     print(removed)
 
 
-def get_surrounding_rolls(x: int, y: int, lines: list[list[str]]) -> list[tuple[int, int]]:
+def get_neighbor_rolls(x: int, y: int, lines: list[list[str]]) -> list[tuple[int, int]]:
     coords: list[tuple[int, int]] = []
 
-    if x > 0:
-        if lines[y][x - 1] == "@":
-            coords.append((x - 1, y))
-    if x < len(lines[y]) - 1:
-        if lines[y][x + 1] == "@":
-            coords.append((x + 1, y))
-    if y > 0:
-        if lines[y - 1][x] == "@":
-            coords.append((x, y - 1))
-        if x > 0 and lines[y - 1][x - 1] == "@":
-            coords.append((x - 1, y - 1))
-        if x < len(lines[y]) - 1 and lines[y - 1][x + 1] == "@":
-            coords.append((x + 1, y - 1))
-    if y < len(lines) - 1:
-        if lines[y + 1][x] == "@":
-            coords.append((x, y + 1))
-        if x > 0 and lines[y + 1][x - 1] == "@":
-            coords.append((x - 1, y + 1))
-        if x < len(lines[y]) - 1 and lines[y + 1][x + 1] == "@":
-            coords.append((x + 1, y + 1))
+    for j in range(y - 1, y + 2):
+        if j < 0 or j >= len(lines):
+            continue
+
+        for i in range(x - 1, x + 2):
+            if i < 0 or i >= len(lines[j]):
+                continue
+            elif i == x and j == y:
+                continue
+
+            if lines[j][i] == "@":
+                coords.append((i, j))
 
     return coords
 
